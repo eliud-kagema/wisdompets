09934e5a89452f1d1321da638e5a837bb634ca1f
Eliud Kagema <eliud.k.maina@gmail.com>
2020-07-29 11:32:06 +0300

    creating models

--- adoptions/models.py
+++ adoptions/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 class Pet(models.Model):
     SEX_CHOICES = [('M', 'Male'), ('F', 'Female')]
-	name = models.CharField(max_length=100)
-	submitter = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    submitter = models.CharField(max_length=100)
     species = models.CharField(max_length=20)
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
-	submission_date = models.DateTimeField()
-	description = models.TextField(blank=True)
+    submission_date = models.DateTimeField()
+    description = models.TextField(blank=True)
     sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
